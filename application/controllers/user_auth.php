f7c72d7513b03fc3a693482f3412404b8a62b659
andikanugraha11 <m.andika.nugraha@gmail.com>
2017-01-16 21:55:14 +0700

    fix google auth

--- application/controllers/user_auth.php
+++ application/controllers/user_auth.php
@@ -3,63 +3,55 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User_auth extends CI_Controller{
 
-	public function __construct() {
-parent::__construct();
+	public function index()
+	{
+
+		// $clientId ='627484450598-bif69q62j2tv83762o8n468m13df5he0.apps.googleusercontent.com';
+		// $clientSecret ='woLIQbgXvQu6BY4YRZ3bK0gS';
+		// $redirectUrl = 'http://localhost/belajarRD/user_auth';
+		$this->load->library('google');
+		$client = new Google_Client();
+		// $client->setApplicationName('Login sob');
+		$client->setAuthConfig(APPPATH."/libraries/google-api-php-client/credential.json");
+		$client->addScope("https://www.googleapis.com/auth/userinfo.email");
+
+		// $client->setClientId($clientId);
+		// $client->setClientSecret($clientSecret);
+		// $client->setRedirectUri($redirectUrl);
+		$service = new Google_Service_Oauth2($client);
+
+		// Add Access Token to Session
+		if (isset($_GET['code']))
+		{
+			$client->authenticate($_GET['code']);
+			$_SESSION['access_token'] = $client->getAccessToken();
+			//header('Location: ' . filter_var($redirectUrl, FILTER_SANITIZE_URL));
+		}
+
+		// Set Access Token to make Request
+		if (isset($_SESSION['access_token']) && $_SESSION['access_token'])
+		{
+			$client->setAccessToken($_SESSION['access_token']);
+		}
+
+		// Get User Data from Google and store them in $data
+		if ($client->getAccessToken())
+		{
+			$userData = $service->userinfo->get();
+			$data['userData'] = $userData;
+			$_SESSION['access_token'] = $client->getAccessToken();
+			} else {
+			$authUrl = $client->createAuthUrl();
+			$data['authUrl'] = $authUrl;
+		}
+
+		$this->load->view('auth', $data);
 }
 
-public function index() {
+		// Unset session and logout
+		public function logout() {
+		unset($_SESSION['access_token']);
+		redirect(base_url());
+		}
 
-// Include two files from google-php-client library in controller
-include_once APPPATH . "libraries/google-api-php-client-master/src/Google/Client.php";
-include_once APPPATH . "libraries/google-api-php-client-master/src/Google/Service/Oauth2.php";
-
-// Store values in variables from project created in Google Developer Console
-$client_id = '< Generated Client Id >';
-$client_secret = '< Generated Client secret Key >';
-$redirect_uri = 'http://localhost/ci_google_oauth/';
-$simple_api_key = '< Generated Server/API Key >';
-
-// Create Client Request to access Google API
-$client = new Google_Client();
-$client->setApplicationName("PHP Google OAuth Login Example");
-$client->setClientId($client_id);
-$client->setClientSecret($client_secret);
-$client->setRedirectUri($redirect_uri);
-$client->setDeveloperKey($simple_api_key);
-$client->addScope("https://www.googleapis.com/auth/userinfo.email");
-
-// Send Client Request
-$objOAuthService = new Google_Service_Oauth2($client);
-
-// Add Access Token to Session
-if (isset($_GET['code'])) {
-$client->authenticate($_GET['code']);
-$_SESSION['access_token'] = $client->getAccessToken();
-header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
-
-// Set Access Token to make Request
-if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
-$client->setAccessToken($_SESSION['access_token']);
-}
-
-// Get User Data from Google and store them in $data
-if ($client->getAccessToken()) {
-$userData = $objOAuthService->userinfo->get();
-$data['userData'] = $userData;
-$_SESSION['access_token'] = $client->getAccessToken();
-} else {
-$authUrl = $client->createAuthUrl();
-$data['authUrl'] = $authUrl;
-}
-// Load view and send values stored in $data
-$this->load->view('google_authentication', $data);
-}
-
-// Unset session and logout
-public function logout() {
-unset($_SESSION['access_token']);
-redirect(base_url());
-}
-}
-?>
--- application/libraries/google.php
+++ application/libraries/google.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
-set_include_path(APPPATH . 'libraries/google-api-php-client-master/src/Google/service' . PATH_SEPARATOR . get_include_path());
-	require_once APPPATH . 'libraries/google-api-php-client-master/src/Google/Client.php';
+set_include_path(APPPATH . 'libraries/google-api-php-client/src/Google/service' . PATH_SEPARATOR . get_include_path());
+	require_once APPPATH . 'libraries/google-api-php-client/vendor/Autoload.php';
 
 class Google extends Google_Client {
     function __construct($params = array()) {
