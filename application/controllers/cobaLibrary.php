f4fbf8a956f16bf5d5f2094bb4c5d10d810dd192
AndikaNugraha11 <m.andika.nugraha@gmail.com>
2017-02-12 13:42:18 +0700

    Buat Library

---
+++ application/controllers/cobaLibrary.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+* 
+*/
+class cobaLibrary extends CI_Controller
+{
+	
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->library('dikalib');
+		
+	}
+
+	public function nama(){
+		
+		$this->dikalib->nama_saya();
+
+	}
+}
---
+++ application/libraries/DikaLib.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+* 
+*/
+class DikaLib
+{
+
+	public function nama_saya(){
+		echo "Muhammad Andika Nugraha";
+	}
+
+	public function umur($umur){
+		echo "Umur saya $umur";
+	}
+}
